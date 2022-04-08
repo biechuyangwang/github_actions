5b61f873e627dae47fed6ef1fe5ee2b0550c41e2
YangWang.BC <1101049446@qq.com>
2022-04-08 10:19:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from PIL import Image
 from io import BytesIO
     
 if __name__ == '__main__':
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+    }
     start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-    response1 = requests.get(start_url, timeout=5)
+    response1 = requests.get(start_url, headers=headers, timeout=5)
     url = "https://www.bing.com" + response1.json()['images'][0]['urlbase'] + "_UHD.jpg"
     filename = response1.json()['images'][0]['copyright'].split(' ')[0]
-    response2 = requests.get(url, timeout=5)
+    response2 = requests.get(url, headers=headers, timeout=5)
     image = Image.open(BytesIO(response2.content))
     image.save('./{}.jpg'.format(filename))
+    print(filename)
