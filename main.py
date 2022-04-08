c8c14490fd86351c3d5cf3ba108b951236802514
YangWang.BC <1101049446@qq.com>
2022-04-08 10:56:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
     response1 = requests.get(start_url, headers=headers, timeout=5)
     # for i in range(10):
-    url = "https://www.bing.com" + response1.json()['images'][i]['urlbase'] + "_UHD.jpg"
-    file_name = response1.json()['images'][1]['copyright'].split('(')[0]
+    url = "https://www.bing.com" + response1.json()['images'][0]['urlbase'] + "_UHD.jpg"
+    file_name = response1.json()['images'][0]['copyright'].split('(')[0]
     response2 = requests.get(url, headers=headers, timeout=5)
     image = Image.open(BytesIO(response2.content))
     image.save('./{}.jpg'.format(file_name))
