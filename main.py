2710cd2ced7cc28bd855e78b08034786fedafc64
YangWang.BC <1101049446@qq.com>
2022-04-08 11:12:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import requests
 from PIL import Image
 from io import BytesIO
+import os
     
 if __name__ == '__main__':
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
     }
-    start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-    response1 = requests.get(start_url, headers=headers, timeout=5)
-    for i in range(10):
+    start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8"
+    response1 = requests.get(start_url, headers=headers, timeout=2)
+    for i in range(8):
         url = "https://www.bing.com" + response1.json()['images'][i]['urlbase'] + "_UHD.jpg"
-        filename = response1.json()['images'][i]['copyright'].split('(')[0]
-        filepath = './img/' + filename + '.jpg'
+        filename = response1.json()['images'][i]['copyright'].split('(')[0].strip()
+        rootpath = './img/'
+        if os.path.exists(rootpath) == False:
+            os.mkdir(rootpath)
+        filepath = rootpath + filename + '.jpg'
         if os.path.exists(filepath):
             continue
-        response2 = requests.get(url, headers=headers, timeout=5)
+        response2 = requests.get(url, headers=headers, timeout=2)
         image = Image.open(BytesIO(response2.content))
         image.save(filepath)
         print(filepath)
