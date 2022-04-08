a5cc97f78eb01a089c5362b361e04625726e1711
YangWang.BC <1101049446@qq.com>
2022-04-08 09:55:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
 import requests
 from PIL import Image
 from io import BytesIO
-
-def make_print_to_file(path='./'): # 标准输出重定向
-    '''
-    path， it is a path for save your log about fuction print
-    example:
-    use  make_print_to_file()   and the   all the information of funtion print , will be write in to a log file
-    :return:
-    '''
-    import sys
-    import os
-    # import config_file as cfg_file
-    import sys
-    import datetime
- 
-    class Logger(object):
-        def __init__(self, filename="Default.log", path="./"):
-            self.terminal = sys.stdout
-            import os
-            if not os.path.exists(path): # 判断文件夹是否存在，不存在则创建文件夹
-                os.mkdir(path)
-            self.log = open(os.path.join(path, filename), "a", encoding='utf8',)
- 
-        def write(self, message):
-            self.terminal.write(message)
-            self.log.write(message)
-
-        def flush(self):
-            pass
-
-    fileName = datetime.datetime.now().strftime('log_'+'%Y_%m_%d_%H')
-    sys.stdout = Logger(fileName + '.md', path=path)
     
 if __name__ == '__main__':
-    make_print_to_file(path='./log/') # 重定型print
-    # print('Hello github')
     start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
-    response1 = requests.get(start_url)
+    response1 = requests.get(start_url, timeout=5)
     url = "https://www.bing.com" + response1.json()['images'][0]['urlbase'] + "_UHD.jpg"
     filename = response1.json()['images'][0]['copyright'].split(' ')[0]
-    response2 = requests.get(url)
+    response2 = requests.get(url, timeout=5)
     image = Image.open(BytesIO(response2.content))
     image.save('./{}.jpg'.format(filename))
