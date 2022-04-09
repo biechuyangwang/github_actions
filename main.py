2d7e8712ec71600e5758527ebed5ff53a5c8302a
YangWang.BC <1101049446@qq.com>
2022-04-10 04:10:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ if __name__ == '__main__':
     headers = {
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
     }
-    start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8"
+    # start_url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8"
+    start_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=zh-cn"
     response1 = requests.get(start_url, headers=headers, timeout=2)
-    for i in range(8):
+    num = len(response1.json()['images'])
+    for i in range(num):
         url = "https://www.bing.com" + response1.json()['images'][i]['urlbase'] + "_UHD.jpg"
         filename = response1.json()['images'][i]['copyright'].split('(')[0].strip()
         rootpath = './img/'
